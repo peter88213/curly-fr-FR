150f1c2257edc1900ba47c630d61ce6fd54f9cff
peter88213 <peter.88213@t-online.de>
2021-03-31 19:48:43 +0200

    Update publish.bat

--- tools/publish.bat
+++ tools/publish.bat
@@ -1,6 +1,15 @@
+REM Prepare a newly compiled version for commit
+
+REM Delete the old version ...
 del ..\*.oxt
 del ..\*.xml
 
+REM Copy the update information to the central location
+REM where old installations might look for it ...
+copy /Y ..\oxt\*.xml ..\..\curly\oxt-fr-FR\
+
+REM Put the new version to the official update location ...
 move ..\oxt\*.oxt ..\
 move ..\oxt\*.xml ..\
 
+pause
