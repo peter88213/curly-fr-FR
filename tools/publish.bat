78ce835d85deb8854d37ee4cb81b486764059629
peter88213 <peter.88213@t-online.de>
2021-04-02 13:02:55 +0200

    Update v3.2.0
    * Add a direct download link to the project home page.

--- tools/publish.bat
+++ tools/publish.bat
@@ -1,5 +1,7 @@
 REM Prepare a newly compiled version for commit
 
+set _version=3.2.0
+
 REM Delete the old version ...
 del ..\*.oxt
 del ..\*.xml
@@ -11,5 +13,9 @@ copy /Y ..\oxt\*.xml ..\..\curly\oxt-fr-FR\
 REM Put the new version to the official update location ...
 move ..\oxt\*.oxt ..\
 move ..\oxt\*.xml ..\
+
+REM Copy the home page template and set the version
+copy ..\docs\template\index.md ..\docs
+python set_version.py %_version%
 
 pause
