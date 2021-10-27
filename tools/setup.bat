031733f9aec5487855e84e367e1db9eb9a6583ab
peter88213 <peter.88213@t-online.de>
2021-10-27 13:29:27 +0200

    Prepare v3.3.0 development version based on curly v3.3.1
    Restructure the whole "curly" project for ant-based build process.

--- tools/setup.bat
+++
@@ -1,3 +0,0 @@
-xcopy /Y /s ..\..\curly\oxt ..\oxt\
-copy /Y ..\..\curly\src\*.xba ..\oxt\curly_fr-FR\
-
--- tools/teardown.bat
+++
@@ -1,6 +0,0 @@
-rmdir /S /Q ..\oxt\Legal
-rmdir /S /Q ..\oxt\help
-rmdir /S /Q ..\oxt\Descriptions
-rmdir /S /Q ..\oxt\icons
-del ..\oxt\curly_fr-FR\Common.xba
-del ..\oxt\curly_fr-FR\Revert.xba
